ec4fbf13c2daefb936c2bae7fda13cf39264683f
yueyongyue <yueyongyue@gmail.com>
2021-04-14 10:41:04 +0800

    add const.go

--- common/const.go
+++ common/const.go
@@ -3,3 +3,9 @@ package common
 const (
 	PresetPath = "/preset/api/v1.10/"
 )
+
+type ResponseData struct {
+	Code int         `json:"code"`
+	Data interface{} `json:"data"`
+	Msg  string      `json:"msg"`
+}
